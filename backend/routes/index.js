a2229b72e8f904ca954db2e21fb0a5eeec93e54b
ForkBombGIT <erosdipede@gmail.com>
2021-06-20 14:22:36 -0400

    Update backend API validation

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,40 +9,63 @@ var resume;
 router.post(
   "/api/email",
   [
-    check("origin", "origin failed"),
-    check("name", "Invalid name input").isAlpha().isLength({ min: 2 }).escape(),
-    check("from", "Invalid from input").isEmail().normalizeEmail(),
-    check("subject", "Invalid subject input")
-      .if(body("origin").contains("CONTACT"))
+    check("origin").isIn(["CONTACT","CAREERS"]).withMessage("Must be equal to CONTACT or CAREERS"),
+    check("name")
+      .isLength({ min: 2 })
+      .withMessage("Must be longer than 2")
+      .isAlpha()
+      .withMessage("Must only consist of letters")
+      .escape(),
+    check("from")
+      .isEmail()
+      .withMessage("Invalid email")
+      .normalizeEmail(),
+    check("subject")
+      .if(
+        body("origin")
+          .not()
+          .contains("CAREERS")
+      )
+      .isLength({ min: 2 })
+      .withMessage("Must be longer than 2")
       .isAlphanumeric()
-      .isLength({ min: 2 })
+      .withMessage("Must be alphanumeric")
       .escape(),
-    check("body", "Invalid body input").isLength({ min: 2 }).escape(),
-    check("resumeText", "Invalid resume text")
+    check("body")
+      .isLength({ min: 2 })
+      .withMessage("Must be longer than 2")
+      .escape(),
+    check("resumeText")
       .if(body("origin").contains("CAREERS"))
-      .if(body("resumeFormat").contains("paste"))
+      .if(body("resumeFormat").not().contains("upload"))
       .isLength({ min: 2 })
-      .escape(),
+      .withMessage("Must be longer than 2")
+      .escape()
   ],
   (req, res) => {
+    console.log(req.body);
     //calls form validation and returns error if there is one
     const errors = validationResult(req);
+    console.log(errors);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     //call send email api and return status response
-    sendEmail(req.body, resume, (status,data) => {
+    sendEmail(req.body, resume, (status, data) => {
       res.status(status).send(data);
     });
-    
   }
 );
 
 router.post("/api/resume", (req, res) => {
   //upload resume
-  uploadResume(req, (upload,data) => {
+  uploadResume(req, (upload, data) => {
     //checks if there was an error uploading file
-    if (upload === null) res.send().status(500).send(data);
+    if (upload === null)
+      res
+        .send()
+        .status(500)
+        .send(data);
     //update resume
     resume = upload;
     //send response
