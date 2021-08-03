c08e941d1992d7ac756a3f2e8ac90425e198a34e
Josh <19962651+Liannus@users.noreply.github.com>
2021-08-03 00:09:15 -0400

    Update Setup Certs to use Docker Restart

--- .ebextensions/setup_certs.sh
+++ .ebextensions/setup_certs.sh
@@ -6,10 +6,10 @@ yum -y install socat
 mkdir /etc/letsencrypt/live/thompsonboilerworks.com -p
 
 #Download the new certificates
-docker stop $(docker ps -q --filter ancestor=liannus/tbwsite:reverseproxy)
 CERT = $(bash ~/.acme.sh/acme.sh --issue --standalone -d thompsonboilerworks.com \
 --key-file       /etc/letsencrypt/live/thompsonboilerworks.com/privkey.pem  \
 --fullchain-file /etc/letsencrypt/live/thompsonboilerworks.com/fullchain.pem)
+docker restart $(docker ps -q --filter ancestor=liannus/tbwsite:reverseproxy)
 
 if [[ CERT -eq 0 || CERT -eq 2 ]]
 then 
