c6ff22f8bd9fe48204575478065b9ba9d0511b50
Eros Di Pede <erosdipede@gmail.com>
2022-02-07 18:51:33 -0500

    Add gradients to PageHeader

--- reverseproxy/scripts/docker-entrypoint.sh
+++ reverseproxy/scripts/docker-entrypoint.sh
@@ -2,12 +2,7 @@
 
 set -e
 
-if [[ ${NODE_ENV} == "development" ]];
-    then 
-        export AUTH_BASIC_VALUE=off
-else 
-    export AUTH_BASIC_VALUE="Restricted"
-fi;
+export AUTH_BASIC_VALUE=off
 
 envsubst '${AUTH_BASIC_VALUE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
